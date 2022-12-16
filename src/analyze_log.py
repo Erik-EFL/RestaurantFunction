532c2bae75ffa00336b4a6e4ccb8ed3e18b8b4b5
erik-efl <erik.lima.f@outlook.com>
2022-12-16 17:13:39 -0300

    feat: :sparkles: Create a new function
    Function write_file
    
    this function is responsible for writing and returning the information requested by the restaurant

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -14,3 +14,18 @@ def read_file(path_to_file: str, trackOrders: TrackOrders) -> list:
         raise FileNotFoundError(f'Arquivo inexistente: {path_to_file}')
 
 
+def write_file(content: str) -> None:
+    most_ordered_dish = content.get_most_ordered_dish_per_customer("maria")
+    quantity_ordered = content.quantity_ordered("arnaldo", "hamburguer")
+    never_ordered = content.get_never_ordered_per_customer("joao")
+    days_never_visited = content.get_days_never_visited_per_customer("joao")
+
+    with open('data/mkt_campaign.txt', "w") as file:
+        file.write(
+            f"{most_ordered_dish}\n"
+            f"{quantity_ordered}\n"
+            f"{never_ordered}\n"
+            f"{days_never_visited}\n"
+        )
+
+
