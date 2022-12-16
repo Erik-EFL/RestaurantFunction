98b5e68b52e68a5026e62970ab995b32e107028a
erik-efl <erik.lima.f@outlook.com>
2022-12-16 17:11:40 -0300

    feat: :sparkles: Create a new function
    Function read file
    
    this function is responsible for reading the files that will be used, checking if the extension is valid and if the file exists

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,16 @@
 import csv
 from track_orders import TrackOrders
+
+
+def read_file(path_to_file: str, trackOrders: TrackOrders) -> list:
+    if not path_to_file.endswith(".csv"):
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+    try:
+        with open(path_to_file) as file:
+            csv_reader = csv.reader(file, delimiter=",")
+            for customer, order, day in csv_reader:
+                trackOrders.add_new_order(customer, order, day)
+    except FileNotFoundError:
+        raise FileNotFoundError(f'Arquivo inexistente: {path_to_file}')
+
+
