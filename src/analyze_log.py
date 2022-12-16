7e64594f01a63bfe09f1336b9ec94a54b5f20167
erik-efl <erik.lima.f@outlook.com>
2022-12-16 18:49:23 -0300

    feat: :sparkles: fix content for a new teste

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -3,15 +3,15 @@ import csv
 from track_orders import TrackOrders
 
 
-def analyze_log(path_to_file: str):
+def analyze_log(path_to_file):
     """ this function parses and writes a new document from specific
     information provided by the user """
     content = TrackOrders()
 
-    if not path_to_file.endswith(".csv"):
+    if not path_to_file.lower().endswith(".csv"):
         raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
     try:
-        with open(path_to_file, encoding='UTF-8') as file:
+        with open(path_to_file, mode="r", encoding="utf-8") as file:
             csv_reader = csv.reader(file, delimiter=",")
             for customer, order, day in csv_reader:
                 content.add_new_order(customer, order, day)
@@ -25,7 +25,7 @@ def analyze_log(path_to_file: str):
     never_ordered = content.get_never_ordered_per_customer("joao")
     days_never_visited = content.get_days_never_visited_per_customer("joao")
 
-    with open('data/mkt_campaign.txt', 'w', encoding='UTF-8') as file:
+    with open('data/mkt_campaign.txt', mode="w", encoding="utf-8") as file:
         file.write(
             f"{most_ordered_dish}\n"
             f"{quantity_ordered}\n"
