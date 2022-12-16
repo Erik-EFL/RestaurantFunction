58a4f9f855997a059e0e12e8a4b9ab608b193703
erik-efl <erik.lima.f@outlook.com>
2022-12-16 19:39:15 -0300

    feat: :sparkles: implementing a new formating function

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,18 +1,52 @@
-"""
-    import csv
-"""
+""" import csv """
 import csv
-
-from track_orders import TrackOrders
+from src.track_orders import TrackOrders
 
 
 def analyze_log(path_to_file):
     """
-        this function parses and writes a new document from specific
-        information provided by the user
-    """
-    content = TrackOrders()
+    Analyzes a CSV log file and writes the results to a new file.
 
+    Args:
+        path_to_file: The path to the CSV log file to analyze.
+        customer: The name of the customer to analyze.
+        dish: The name of the dish to analyze.
+
+    Returns:
+        None.
+    """
+    # Read the log file and create a TrackOrders object
+    content = TrackOrders()
+    read_log_file(path_to_file, content)
+
+    # Analyze the data and write the results to a new file
+    with open('data/mkt_campaign.txt', mode="w", encoding="utf-8") as file:
+        most_ordered_dish = content.get_most_ordered_dish_per_customer("maria")
+        quantity_ordered = content.quantity_ordered("arnaldo", "hamburguer")
+        never_ordered = content.get_never_ordered_per_customer("joao")
+        days_never_visited = content.get_days_never_visited_per_customer(
+            "joao"
+        )
+
+        file.write(
+            f"{most_ordered_dish}\n"
+            f"{quantity_ordered}\n"
+            f"{never_ordered}\n"
+            f"{days_never_visited}\n"
+        )
+
+
+def read_log_file(path_to_file, content):
+    """
+    Reads a CSV log file and adds the data to a TrackOrders object.
+
+    Args:
+        path_to_file: The path to the CSV log file to read.
+        content: The TrackOrders object
+
+    Returns:
+        None.
+    """
     if not path_to_file.lower().endswith(".csv"):
         raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
     try:
@@ -24,16 +58,3 @@ def analyze_log(path_to_file):
         raise FileNotFoundError(
             f"Arquivo inexistente: {path_to_file}"
         ) from err
-
-    most_ordered_dish = content.get_most_ordered_dish_per_customer("maria")
-    quantity_ordered = content.quantity_ordered("arnaldo", "hamburguer")
-    never_ordered = content.get_never_ordered_per_customer("joao")
-    days_never_visited = content.get_days_never_visited_per_customer("joao")
-
-    with open('data/mkt_campaign.txt', mode="w", encoding="utf-8") as file:
-        file.write(
-            f"{most_ordered_dish}\n"
-            f"{quantity_ordered}\n"
-            f"{never_ordered}\n"
-            f"{days_never_visited}\n"
-        )
